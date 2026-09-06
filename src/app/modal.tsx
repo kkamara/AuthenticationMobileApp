69c748427c9ca6f60a71b1a7e498d5310a28eeed
kkamara <kamaracomputers@gmail.com>
2026-09-06 14:58:35 +0100

    Update modal display title

--- src/app/modal.tsx
+++ src/app/modal.tsx
@@ -6,7 +6,7 @@ import { Text, View } from '@/components/Themed';
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>Modal Screen</Text>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
