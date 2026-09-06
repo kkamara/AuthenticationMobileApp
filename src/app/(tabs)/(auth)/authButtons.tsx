112087faaf55f70a83b64ad55d95c0b8f357694d
kkamara <kamaracomputers@gmail.com>
2026-09-06 15:20:59 +0100

    Design Auth Buttons Screen

--- src/app/(tabs)/(auth)/authButtons.tsx
+++ src/app/(tabs)/(auth)/authButtons.tsx
@@ -1,10 +1,20 @@
-import { Text, View, } from '@/components/Themed';
+import Button from '@/components/Button';
+import { View, } from '@/components/Themed';
 import { StyleSheet, } from 'react-native';
 
 export default function AuthButtonsScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Auth Buttons Screen</Text>
+      <Button
+        pressableStyle={styles.button}
+        textStyle={styles.buttonText}
+        text="Login"
+      />
+      <Button
+        pressableStyle={styles.button}
+        textStyle={styles.buttonText}
+        text="Register"
+      />
     </View>
   );
 }
@@ -19,5 +29,12 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  button: {
+    marginBottom: 20,
+    width: 400,
+  },
+  buttonText: {
+    fontSize: 28,
   },
 });
